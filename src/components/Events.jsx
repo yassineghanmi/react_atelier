cf0c760a5b8f94ce031c2de3b87c4718ab038d06
Armi64bit <bahaeddine.bouzid@esprit.tn>
2024-02-05 12:04:00 +0100

    Update Events.jsx

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -11,7 +11,7 @@ function Events() {
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
-      setError(null); // Clear any previous errors
+      setError(null); 
 
       try {
         const response = await axios.get('src/data/events.json');
@@ -27,7 +27,6 @@ function Events() {
   }, []);
   const handleBookEvent = async (eventId) => {
     try {
-      // Simulate updating data in a real-world scenario (API call, etc.)
       const updatedData = data.map((item) =>
         item.id === eventId
           ? { ...item, nbParticipants: item.nbParticipants + 1, nbTickets: item.nbTickets - 1 }
@@ -35,9 +34,7 @@ function Events() {
       );
       setData(updatedData);
     } catch (error) {
-      // Handle any errors during booking
       console.error("Error booking event:", error);
-      // Display an error message to the user
     }
   };
   return (
@@ -45,11 +42,11 @@ function Events() {
        
           {data.map((item) => (
             <Event
-              key={item.id} // Assuming each event has a unique 'id' property
+              key={item.id} 
               name={item.name} 
               price={item.price}
               nbTickets={item.nbTickets}
-              nbParticipants={item.nbParticipants} // Uncomment to display nbofp
+              nbParticipants={item.nbParticipants} 
               description={item.description}
               img={item.img}
               handleBookEvent={handleBookEvent}
